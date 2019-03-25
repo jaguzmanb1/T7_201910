e03d42d9c86677d4d0deca6fdee9f10812f0d24e
unknown <ja.guzmanb1@uniandes.edu.co>
2019-03-24 23:49:19 -0500

    P2

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -23,19 +23,19 @@ import model.vo.VOMovingViolation;
 import view.MovingViolationsManagerView;
 
 @SuppressWarnings("unused")
-public class Controller 
+public class Controller
 {
-	private MovingViolationsManagerView view ; 
+	private MovingViolationsManagerView view ;
 
 	// TODO Definir las estructuras de datos para cargar las infracciones del periodo definido
-	private Queue<VOMovingViolation> colaInfracciones ; 
+	private Queue<VOMovingViolation> colaInfracciones ;
 
-	// Muestra obtenida de los datos cargados 
-	Comparable<VOMovingViolation> [ ] muestra ; 
+	// Muestra obtenida de los datos cargados
+	Comparable<VOMovingViolation> [ ] muestra ;
 
-	// Copia de la muestra de datos a ordenar 
-	Comparable<VOMovingViolation> [ ] muestraCopia ; 
-	
+	// Copia de la muestra de datos a ordenar
+	Comparable<VOMovingViolation> [ ] muestraCopia ;
+
 	ColaPrioridad<String, VOMovingViolation> colaPrioridad;
 
 	private String[] listaMes;
@@ -44,7 +44,7 @@ public class Controller
 		view = new MovingViolationsManagerView();
 
 		listaMes = new String[]{"January" , "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		colaInfracciones  =  new Queue<VOMovingViolation>() ; 
+		colaInfracciones  =  new Queue<VOMovingViolation>() ;
 		colaPrioridad = new ColaPrioridad<String, VOMovingViolation>();
 	}
 
@@ -53,7 +53,7 @@ public class Controller
 	 * Leer los datos de las infracciones de los archivos. Cada infraccion debe ser Comparable para ser usada en los ordenamientos.
 	 * Todas infracciones (MovingViolation) deben almacenarse en una Estructura de Datos (en el mismo orden como estan los archivos)
 	 * A partir de estos datos se obtendran muestras para evaluar los algoritmos de ordenamiento
-	 * @return numero de infracciones leidas 
+	 * @return numero de infracciones leidas
 	 */
 	/**
 	 * Leer los datos de las infracciones de los archivos. Cada infraccion debe ser Comparable para ser usada en los ordenamientos.
@@ -62,11 +62,11 @@ public class Controller
 	 * @return numero de infracciones leidas rden como estan los
 	 */
 
-	public int loadMovingViolations(int pSemestre) 
+	public int loadMovingViolations(int pSemestre)
 	{
 		int limSup;
 		int limInf;
-		
+
 		if ( pSemestre == 1 ){
 			limSup = 6;
 			limInf = 0;
@@ -76,7 +76,7 @@ public class Controller
 			limInf = 6;
 		}
 		String dataFile;
-		
+
 		try{
 			for(int f = limInf ; f < limSup ; f++){
 				view.printMensage(listaMes[f]);
@@ -88,7 +88,7 @@ public class Controller
 				for(int j = 0 ; j < info.size() ; j++){
 					VOMovingViolation infraccion = new VOMovingViolation(info.get(f)[0], info.get(f)[2], info.get(f)[13],info.get(f)[9], info.get(f)[12], info.get(f)[15]);
 					colaPrioridad.insertar(infraccion.getAccidentIndicator(), infraccion);
-					//colaInfracciones.enqueue(infraccion);	
+					//colaInfracciones.enqueue(infraccion);
 
 					}
 				n1.close();
@@ -96,7 +96,7 @@ public class Controller
 
 			}
 		}
-		catch (Exception e) 
+		catch (Exception e)
 		{
 			// TODO: handle exception
 			e.printStackTrace();
@@ -110,15 +110,8 @@ public class Controller
 
 
 
-	public void run() 
+	public void run()
 	{
-		long startTime;
-		long endTime;
-		long duration;
-
-		int nDatos = 0;
-		int nMuestra = 0;
-
 
 		Scanner sc = new Scanner(System.in);
 		boolean fin = false;
@@ -137,7 +130,7 @@ public class Controller
 				Scanner semestre = new Scanner(System.in);
 				int semestren = semestre.nextInt();
 				nDatos = this.loadMovingViolations(semestren);
-				
+
 				view.printMensage("Numero infracciones cargadas:" + nDatos);
 				break;
 
@@ -159,13 +152,13 @@ public class Controller
 			case 7:
 				break;
 
-			case 8:	
+			case 8:
 				break;
 
 			case 9:
 				break;
 
-			case 10:	
+			case 10:
 				fin=true;
 				sc.close();
 				break;
